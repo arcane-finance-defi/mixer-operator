d03561bee457bff8c318862d3c9ab37e723e3d3a
Maxim Sultakov <soultakov@gmail.com>
2025-05-15 14:25:22 +0300

    feat: add rpc

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::ops::Deref;
-use std::sync::Arc;
+use crate::config::Config;
 use dotenv::dotenv;
-use rocket::{get, routes};
+use log::{error, info};
+use rocket::State as RocketState;
 use rocket::http::Status;
 use rocket::serde::json::Json;
+use rocket::{get, routes};
+use rpc::MidenRpcAsyncFacade;
+use rpc::ThreadPoolMidenRpcAsyncFacade;
 use serde::{Deserialize, Serialize};
-use crate::config::Config;
-use rocket::State as RocketState;
+use std::sync::Arc;
 #[macro_use]
 extern crate rocket;
 #[macro_use]
@@ -14,6 +16,10 @@ extern crate alloc;
 extern crate core;
 
 mod config;
+mod rpc;
+struct State {
+    rpc: Arc<ThreadPoolMidenRpcAsyncFacade>,
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -21,25 +27,44 @@ pub struct ErrorResponse {
     error: String,
 }
 
-#[get("/chain-tip")]
-async fn chain_tip() -> Result<(), Status> {
-    todo!()
+#[get("/block-number")]
+async fn block_number(state: &RocketState<State>) -> Result<Json<u32>, Status> {
+    let rpc = state.rpc.clone();
+
+    match rpc.get_block_header(None, false).await {
+        Ok((header, _)) => {
+            info!(
+                "Successfully retrieved block header with number: {}",
+                header.block_num().as_u32()
+            );
+            Ok(Json::from(header.block_num().as_u32()))
+        }
+        Err(e) => {
+            error!("Failed to get block header: {}", e);
+            Err(Status::InternalServerError)
+        }
+    }
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[rocket::launch]
+fn rocket() -> _ {
     dotenv().ok();
+    env_logger::init();
 
     let rocket = rocket::build();
-
     let figment = rocket.figment();
     let config: Config = figment.extract().expect("config");
 
-    rocket
-        .mount("/", routes![chain_tip])
-        .launch()
-        .await
-        .unwrap();
+    let endpoint = miden_client::rpc::Endpoint::new("http".to_string(), config.rpc_url(), None);
+    let client_count = config.client_count();
 
-    Ok(())
+    let miden_facade = Arc::new(ThreadPoolMidenRpcAsyncFacade::new(
+        client_count,
+        &endpoint,
+        config.rpc_timeout_ms(),
+    ));
+
+    let state = State { rpc: miden_facade };
+
+    rocket.manage(state).mount("/", routes![block_number])
 }
---
+++ src/rpc/mod.rs
@@ -0,0 +1,7 @@
+mod common;
+mod errors;
+mod requests;
+
+pub use common::MidenRpcAsyncFacade;
+pub use common::ThreadPoolMidenRpcAsyncFacade;
+pub use errors::MidenFacadeRpcError;
