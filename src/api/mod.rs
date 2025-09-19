6418c7244794d72ad02a127a8a0ebbaa07978bd6
Vladislav Kochetkov <vakochetkov3391@gmail.com>
2025-09-19 03:01:03 +0300

    feat: routes returning function

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,17 @@
 mod error;
 pub mod mix;
 pub mod note_drafts;
+
+pub fn routes() -> Vec<rocket::Route> {
+    rocket::routes![
+        mix::post_handler,
+        mix::post_batch_handler,
+        mix::delayed_post_handler,
+        mix::delayed_status_get_handler,
+        note_drafts::post_new_handler,
+        note_drafts::get_status_handler,
+        // api::note_drafts::get_by_id_handler,
+        // api::note_drafts::post_activate_by_id_handler,
+        // api::note_drafts::delete_by_id_handler,
+    ]
+}
